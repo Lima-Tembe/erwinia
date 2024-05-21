adab5a70e773662100b5725162f42ee5a8b07ae0
Lima Tembe <limatembe44@gmail.com>
2024-05-21 12:32:09 +0200

    Finalizado o treatment screen e os testes

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,30 +1,255 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility in the flutter_test package. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:erwinia/main.dart';
-
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+  group("Widget Test (Home Screen)- ", () {
+    testWidgets('Testando o acesso a camera do celular quando permissão for aceite...',
+        (WidgetTester tester) async {
+      // Create the widget under test
+      await tester.pumpWidget(const MaterialApp(
+        home: Scaffold(
+          body: Center(
+            child: Text('Hello, World!'),
+          ),
+        ),
+      ));
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+      // Verify that the Text widget displays the correct text
+      expect(find.text('Hello, World!'), findsOneWidget);
+    });
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
+    testWidgets('Testando o acesso a camera do celular quando a permissão for rejeitada...',
+        (WidgetTester tester) async {
+      // Create the widget under test
+      await tester.pumpWidget(const MaterialApp(
+        home: Scaffold(
+          body: Center(
+            child: Text('Hello, World!'),
+          ),
+        ),
+      ));
 
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+      // Verify that the Text widget displays the correct text
+      expect(find.text('Hello, World!'), findsOneWidget);
+    });
+
+    testWidgets(
+        'Testando as diferente opções do flash...',
+        (WidgetTester tester) async {
+      // Create the widget under test
+      await tester.pumpWidget(const MaterialApp(
+        home: Scaffold(
+          body: Center(
+            child: Text('Hello, World!'),
+          ),
+        ),
+      ));
+
+      // Verify that the Text widget displays the correct text
+      expect(find.text('Hello, World!'), findsOneWidget);
+    });
+
+    testWidgets('Testando a visibilidade do FAB...',
+        (WidgetTester tester) async {
+      // Create the widget under test
+      await tester.pumpWidget(const MaterialApp(
+        home: Scaffold(
+          body: Center(
+            child: Text('Hello, World!'),
+          ),
+        ),
+      ));
+
+      // Verify that the Text widget displays the correct text
+      expect(find.text('Hello, World!'), findsOneWidget);
+    });
+
+    testWidgets('Testando se clicar no FAB se ele vai ao Situation Screen...',
+        (WidgetTester tester) async {
+      // Create the widget under test
+      await tester.pumpWidget(const MaterialApp(
+        home: Scaffold(
+          body: Center(
+            child: Text('Hello, World!'),
+          ),
+        ),
+      ));
+
+      // Verify that the Text widget displays the correct text
+      expect(find.text('Hello, World!'), findsOneWidget);
+    });
+  });
+
+  group("Widget Test (Situation Screen)- ", () {
+    testWidgets(
+        'Testando se aparece a imagem...',
+        (WidgetTester tester) async {
+      // Create the widget under test
+      await tester.pumpWidget(const MaterialApp(
+        home: Scaffold(
+          body: Center(
+            child: Text('Hello, World!'),
+          ),
+        ),
+      ));
+
+      // Verify that the Text widget displays the correct text
+      expect(find.text('Hello, World!'), findsOneWidget);
+    });
+
+    testWidgets(
+        'Testando se aparece o texto quando uma planta está infectada...',
+        (WidgetTester tester) async {
+      // Create the widget under test
+      await tester.pumpWidget(const MaterialApp(
+        home: Scaffold(
+          body: Center(
+            child: Text('Hello, World!'),
+          ),
+        ),
+      ));
+
+      // Verify that the Text widget displays the correct text
+      expect(find.text('Hello, World!'), findsOneWidget);
+    });
+
+    testWidgets('Testando se aparece o texto quando a plata está saudável...',
+        (WidgetTester tester) async {
+      // Create the widget under test
+      await tester.pumpWidget(const MaterialApp(
+        home: Scaffold(
+          body: Center(
+            child: Text('Hello, World!'),
+          ),
+        ),
+      ));
+
+      // Verify that the Text widget displays the correct text
+      expect(find.text('Hello, World!'), findsOneWidget);
+    });
+
+    testWidgets('Testando a visibilidade do FAB de resultado...',
+        (WidgetTester tester) async {
+      // Create the widget under test
+      await tester.pumpWidget(const MaterialApp(
+        home: Scaffold(
+          body: Center(
+            child: Text('Hello, World!'),
+          ),
+        ),
+      ));
+
+      // Verify that the Text widget displays the correct text
+      expect(find.text('Hello, World!'), findsOneWidget);
+    });
+
+    testWidgets('Testando se clicar no FAB se ele vai ao Treatment Screen...',
+        (WidgetTester tester) async {
+      // Create the widget under test
+      await tester.pumpWidget(const MaterialApp(
+        home: Scaffold(
+          body: Center(
+            child: Text('Hello, World!'),
+          ),
+        ),
+      ));
+
+      // Verify that the Text widget displays the correct text
+      expect(find.text('Hello, World!'), findsOneWidget);
+    });
+  });
+
+  group("Widget Test (Treatment Screen)- ", () {
+    testWidgets(
+        'Testando a imagem aparece no processo de tratamento...',
+        (WidgetTester tester) async {
+      // Create the widget under test
+      await tester.pumpWidget(const MaterialApp(
+        home: Scaffold(
+          body: Center(
+            child: Text('Hello, World!'),
+          ),
+        ),
+      ));
+
+      // Verify that the Text widget displays the correct text
+      expect(find.text('Hello, World!'), findsOneWidget);
+    });
+
+    testWidgets(
+        'Testando se aparece o texto de diagnostico...',
+        (WidgetTester tester) async {
+      // Create the widget under test
+      await tester.pumpWidget(const MaterialApp(
+        home: Scaffold(
+          body: Center(
+            child: Text('Hello, World!'),
+          ),
+        ),
+      ));
+
+      // Verify that the Text widget displays the correct text
+      expect(find.text('Hello, World!'), findsOneWidget);
+    });
+
+    testWidgets('Testando se aparece os sintomas...',
+        (WidgetTester tester) async {
+      // Create the widget under test
+      await tester.pumpWidget(const MaterialApp(
+        home: Scaffold(
+          body: Center(
+            child: Text('Hello, World!'),
+          ),
+        ),
+      ));
+
+      // Verify that the Text widget displays the correct text
+      expect(find.text('Hello, World!'), findsOneWidget);
+    });
+
+    testWidgets('Testando as medidas preventivas...',
+        (WidgetTester tester) async {
+      // Create the widget under test
+      await tester.pumpWidget(const MaterialApp(
+        home: Scaffold(
+          body: Center(
+            child: Text('Hello, World!'),
+          ),
+        ),
+      ));
+
+      // Verify that the Text widget displays the correct text
+      expect(find.text('Hello, World!'), findsOneWidget);
+    });
+
+    testWidgets('Testando se aparece o processo de controle/tratamento...',
+        (WidgetTester tester) async {
+      // Create the widget under test
+      await tester.pumpWidget(const MaterialApp(
+        home: Scaffold(
+          body: Center(
+            child: Text('Hello, World!'),
+          ),
+        ),
+      ));
+
+      // Verify that the Text widget displays the correct text
+      expect(find.text('Hello, World!'), findsOneWidget);
+    });
+
+    testWidgets('Testando se salva o tratamento...',
+        (WidgetTester tester) async {
+      // Create the widget under test
+      await tester.pumpWidget(const MaterialApp(
+        home: Scaffold(
+          body: Center(
+            child: Text('Hello, World!'),
+          ),
+        ),
+      ));
+
+      // Verify that the Text widget displays the correct text
+      expect(find.text('Hello, World!'), findsOneWidget);
+    });
   });
 }
